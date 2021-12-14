edd7e450b9933c0e573a6a4a7f6738e891c6d7e1
Roman Fedyashov <fedyashov.r@sparkling.com>
2021-12-14 20:03:03 +0700

    light fix for install scripts

--- software/v1-enlight/install/prepare_pi.sh
+++ software/v1-enlight/install/prepare_pi.sh
@@ -5,6 +5,9 @@ sudo apt-get update -y && sudo apt-get upgrade -y
 sudo apt install -y libssl-dev libsdl-image1.2-dev libevent-dev libsdl1.2-dev libjansson-dev
 sudo ./update_system.sh
 
+#Let's clean up the file first
+: > /home/pi/run.sh
+echo "xrandr -s 1920x1080" >> /home/pi/run.sh
 echo "cd /home/pi/wash" >> /home/pi/run.sh
 echo "./firmware.exe" >> /home/pi/run.sh
 
