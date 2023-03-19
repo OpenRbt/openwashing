3e0412b2203ba12786f0f12109a42141ac19300b
romanidzs <romanidzs@gmail.com>
2023-03-19 20:38:32 +0700

    Update Driver

--- hardware/DriverArduino/YF-S201/FinalDriver_YF-S.ino
+++ hardware/DriverArduino/YF-S201/FinalDriver_YF-S.ino
@@ -7,9 +7,9 @@ float vol = 0.0;
 int stat = IDL;
 unsigned char flowsensor = 2;  // Sensor Input
 unsigned long currentTime, cloopTime;
-int timer = 200, total_count = 10, switch_count = 4, count = 0;
+int timer = 200, timer_vol = 1000, total_count = 10, switch_count = 4, count = 0;
 int state[10];
-int sensor_read_position = 0, status = 0, l_minute, milliliters_finish, number_of_milliliters;
+int sensor_read_position = 0, status = 0, l_minute, milliliters_from_pulses, number_of_milliliters;
 
 int countPin(int *state) {
   int res = 0;
@@ -22,7 +22,7 @@ int countPin(int *state) {
 }
 
 void setup() {
-  pinMode(flowsensor, INPUT_PULLUP);
+  pinMode(flowsensor, INPUT);
   Serial.begin(38400);
 
   currentTime = millis();
@@ -50,11 +50,10 @@ void loop() {
       }
     }
     currentTime = millis();
-    if (milliliters_finish > vol) {
+    if (milliliters_from_pulses > number_of_pulses) {
       if (currentTime >= (cloopTime + timer)) {
         cloopTime = currentTime;
-        vol = number_of_pulses / 7.5 / 60.0 * 1000;
-        Serial.println("P" + (String)(int)(vol) + ";");
+        Serial.println("P" + (String)(int)(number_of_pulses / 500.0 * 1000) + ";");
       }
     } else {
       stat = SENDING_FINISH;
@@ -77,8 +76,8 @@ void loop() {
       stat = LIQUID_ON;
       number_of_pulses = 0;
       status = 0;
-      number_of_milliliters = str.toInt();
-      milliliters_finish = number_of_milliliters / 1000;
+      number_of_milliliters = (float)str.toInt();
+      milliliters_from_pulses = number_of_milliliters / 500;
     } else {
       if (str == "UID;") {
         Serial.print("YF-S201;");
