79c011fb240f90afed0716cb2b634b4859b8b8fb
Roman Fedyashov <rcsrambler@gmail.com>
2023-05-14 18:44:27 +0600

    small fix typo

--- software/v1-enlight/dia_firmware.h
+++ software/v1-enlight/dia_firmware.h
@@ -1,6 +1,6 @@
 #ifndef _DIA_FIRMWARE_H
 #define _DIA_FIRMWARE_H
-#defing DIA_REQUEST_RETRY_ATTEMPTS 50
+#define DIA_REQUEST_RETRY_ATTEMPTS 50
 #include "dia_network.h"
 
 std::string SetRegistryValueByKeyIfNotExists(std::string key, std::string value);
