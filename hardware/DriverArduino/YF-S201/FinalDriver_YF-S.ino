cc595c4cd4fcbc2746867519c4165af4101c4226
arsikirill <98137620+arsikirill@users.noreply.github.com>
2023-07-14 19:58:31 +0700

    Arduino YF-S201 FinalDriver_YF-S (#36)
    * Add driver Arduino
    
    * Add new driver for YF-S201
    
    * Update Driver
    
    * fix
    
    ---------
    
    Co-authored-by: romanidzs <romanidzs@gmail.com>

---
+++ hardware/DriverArduino/YF-S201/FinalDriver_YF-S.ino
@@ -0,0 +1,99 @@
+#define IDL 1
+#define LIQUID_ON 2
+#define SENDING_FINISH 3
+
+volatile int number_of_pulses = 0;
+float vol = 0.0, coef_litr = 1000.0, coef_impuls = 500.0;
+int stat = IDL;
+unsigned char flowsensor = 2;  // Sensor Input
+unsigned long currentTime, cloopTime;
+int timer = 200, timer_vol = 1000, total_count = 10, switch_count = 4, count = 0;
+int state[10];
+int sensor_read_position = 0, status = 0, l_minute, milliliters_from_pulses, number_of_milliliters;
+
+int countPin(int *state) {
+  int res = 0;
+  for (int i = 0; i < total_count; i++) {
+    if (state[i]) {
+      res++;
+    }
+  }
+  return res;
+}
+
+void setup() {
+  pinMode(flowsensor, INPUT);
+  Serial.begin(38400);
+
+  currentTime = millis();
+  cloopTime = currentTime;
+}
+
+void loop() {
+  if (stat == LIQUID_ON) {
+    int cur_state = digitalRead(flowsensor);
+    state[sensor_read_position] = cur_state;
+    sensor_read_position++;
+
+    if (sensor_read_position >= total_count) {
+      sensor_read_position = 0;
+    }
+
+    count = countPin(state);
+
+    if (count > total_count - switch_count) {
+      status = 1;
+    } else {
+      if (count < switch_count && status == 1) {
+        number_of_pulses++;
+        status = 0;
+      }
+    }
+    currentTime = millis();
+    if (milliliters_from_pulses > number_of_pulses) {
+      if (currentTime >= (cloopTime + timer)) {
+        cloopTime = currentTime;
+        Serial.println("P" + (String)(int)(number_of_pulses / coef_impuls * coef_litr) + ";");
+      }
+    } else {
+      stat = SENDING_FINISH;
+    }
+  }
+
+  if (stat == SENDING_FINISH) {
+    Serial.println("F" + (String)(int)number_of_milliliters + ";");
+    stat = IDL;
+  }
+  if (Serial.available() > 0) {
+    String str = Serial.readString();
+    Serial.setTimeout(5);
+    if (str[0] == 'S') {
+      Serial.println("SOK;");
+      str.remove(0, 1);
+      for (int i = 0; i < total_count; i++) {
+        state[i] = 0;        
+      }
+      stat = LIQUID_ON;
+      number_of_pulses = 0;
+      status = 0;
+      number_of_milliliters = (float)str.toInt();
+      milliliters_from_pulses = number_of_milliliters / coef_litr * coef_impuls;
+    } else {
+      if (str == "UID;") {
+        Serial.print("YF-S201;");
+      }
+      if (str == "PING;") {
+        Serial.println("OK-PING;");
+      }
+      if (str == "ERR;") {
+        stat = IDL;
+        Serial.println("FOK;");
+      }
+      if (str == "FOK;") {
+        stat = IDL;
+      }
+    }
+    str = "";
+    Serial.flush();
+  }
+}
